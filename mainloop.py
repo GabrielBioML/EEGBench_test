94427533b8c556ea208da7a2345abe34ebf698ae
GabrielBioML <gab13bag@gmail.com>
2017-05-18 15:14:28 -0400

    update 18 mai 2017 15:14

--- mainloop.py
+++ mainloop.py
@@ -28,34 +28,41 @@ import time
 import glob
 from random import shuffle
 
-def wait(temps):
-	a = time.clock() + temps
-	b = 0
-	while b < a:
-		b = time.clock()
-	return 0
-
 def setupimage(): #load the picture that will be used
-	liste = glob.glob('/home/pi/Documents/EEGBench_test/Images/picture*')
-	n = len(liste)
-	shuffle(liste)
-	d = {}
-	namespace = globals()
-	for i in range (0,n-1):
-		namespace['image_%d' % i] = pygame.image.load(os.path.join(liste[i]))
-	return 0
+		
+		liste = glob.glob('/home/pi/Documents/EEGBench_test/Images/picture*')
+		n = len(liste)
+		image=[None] * n
+		shuffle(liste)
+		#namespace = globals()
+		for i in range (0,n-1):
+			#namespace['image_%d' % i] 
+			image[i]= pygame.image.load(os.path.join(liste[i+1]))
+		return image
+			
+			
+class Picture(object):
+	def __init__(self):
+		self.__picture=setupimage()
+		
+	def wait(self,temps):
+		a = time.clock() + temps
+		b = 0
+		while b < a:
+				b = time.clock()
 
-def main():
-	setupimage()
-	pygame.init()
-	a = time.time()	
-	image = pygame.image.load(os.path.join('/home/pi/Documents/EEGBench_test/Images/picture_1.jpg'))
-	screen = pygame.display.set_mode((1824, 984))
-	screen.blit(image, (0,0))
-	pygame.display.flip()
-	wait(3)
-	return 0 
 
+	def main(self):
+		#self.setupimage()
+		pygame.init()
+		a = time.time()	
+		#image = pygame.image.load(os.path.join('/home/pi/Documents/EEGBench_test/Images/picture_1.jpg'))
+		screen = pygame.display.set_mode((1824, 984))
+		screen.blit(self.__picture[2], (0,0))
+		pygame.display.flip()
+		self.wait(3)
+		
 if __name__ == '__main__':
-	main()
+	pic=Picture()
+	pic.main()
 
